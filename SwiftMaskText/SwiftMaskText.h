4461cfaafb815a7f07af7422b79c101a0316df46
André Santana Ferreira <andre.991@hotmail.com>
2017-06-07 16:10:44 -0300

    Cocoa Pods added

---
+++ SwiftMaskText/SwiftMaskText.h
@@ -0,0 +1,19 @@
+//
+//  SwiftMaskText.h
+//  SwiftMaskText
+//
+//  Created by Andre Santana Ferreira on 07/06/17.
+//  Copyright © 2017 com.moraisandre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftMaskText.
+FOUNDATION_EXPORT double SwiftMaskTextVersionNumber;
+
+//! Project version string for SwiftMaskText.
+FOUNDATION_EXPORT const unsigned char SwiftMaskTextVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftMaskText/PublicHeader.h>
+
+
